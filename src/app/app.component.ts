c1fa44c676977591891f43121de4df418d63580e
kaiching0109 <aaronsuen0109@gmail.com>
2020-08-05 18:33:52 +0900

    INIT: add auth guard and simple redirecting for on auth access

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AuthService } from './auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -13,8 +15,10 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
+    private statusBar: StatusBar,
+    private authService: AuthService,
+    private router: Router
+    ) {
     this.initializeApp();
   }
 
@@ -26,6 +30,7 @@ export class AppComponent {
   }
 
   onLogout() {
-    console.log("Logout is clicked")
+    this.authService.logout();
+    this.router.navigateByUrl('/auth');
   }
 }
