268e0cffa39ecff7c3f7029dcb08315d19fefdd3
kaiching0109 <aaronsuen0109@gmail.com>
2020-08-05 10:05:06 +0900

    INIT: Project Setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,26 +3,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'places',
+    loadChildren: () => import('./places/places.module').then( m => m.PlacesPageModule)
   },
   {
-    path: 'recipes',
-    children: [
-      {
-        path: '',
-        loadChildren: () => import('./recipes/recipes.module').then( m => m.RecipesPageModule),
-      },
-      {
-        path: ':recipeId',
-        loadChildren: () => import('./recipes/recipe-detail/recipe-detail.module').then( m => m.RecipeDetailPageModule),
-      },
-    ]
+    path: 'bookings',
+    loadChildren: () => import('./bookings/bookings.module').then( m => m.BookingsPageModule)
   },
 ];
 
