d1cc8472c6aa75dae27d51dd719a00c761d93021
kaiching0109 <aaronsuen0109@gmail.com>
2020-08-05 10:41:15 +0900

    INIT: ion-tab

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'places',
+    pathMatch: 'full'
+  },
+  {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
   },
