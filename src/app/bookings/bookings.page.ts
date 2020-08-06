e2817a94ea6d73d1d9be060e706e6fcc84e99510
kaiching0109 <aaronsuen0109@gmail.com>
2020-08-06 14:53:50 +0900

    INIT: add sliding elements for offer page to remove offers

--- src/app/bookings/bookings.page.ts
+++ src/app/bookings/bookings.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Booking } from './booking.module';
+import { BookingService } from './booking.service';
+import { IonItemSliding } from '@ionic/angular';
 
 @Component({
   selector: 'app-bookings',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingsPage implements OnInit {
 
-  constructor() { }
+  constructor(private bookingService: BookingService) { }
+
+  loadedBookings: Booking[];
 
   ngOnInit() {
+    this.loadedBookings = this.bookingService.bookings; 
   }
 
+  onCancelBooking(offerId: string, slideEl: IonItemSliding) {
+    slideEl.close()
+    //cancel offerId's offer
+  }
 }
