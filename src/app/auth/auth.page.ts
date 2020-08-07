6eec91225bdcdca9b491ad92a89afeab81501ff0
kaiching0109 <aaronsuen0109@gmail.com>
2020-08-07 13:11:15 +0900

    UPDATE: signin signup form

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -11,6 +12,7 @@ import { LoadingController } from '@ionic/angular';
 export class AuthPage implements OnInit {
 
   isLoading = false;
+  isLogin = true;
 
   constructor(private loadingCtl: LoadingController,private authService: AuthService, private router: Router) { }
 
@@ -30,6 +32,23 @@ export class AuthPage implements OnInit {
           this.router.navigateByUrl('/places/tabs/discover');
         }, 1500)
       });
+  }
+
+  onSubmit(form: NgForm) {
+    if(!form.valid) return 
+    else {
+      const {email, password} = form.value;
+
+      if(this.isLogin) {
+        //Login
+      } else {
+        //Signup
+      }
+    }
+  }
+
+  onSwithcAuthMode() {
+    this.isLogin = !this.isLogin;
   }
 
 }
